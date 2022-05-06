215cfeaa267682bd3ccc6e392b40d88ec64b1f14
Norbu Gyachung <norbuw@gmail.com>
2022-05-06 11:52:01 +0200

    updated contact form

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+// Email 
+function sendEmail() {
+  Email.send({
+      Host: "smtp.gmail.com",
+      Username: "norbuw@gmail.com",
+      Password: "eesfzcemewojfnkn",
+      To: 'norbuw@gmail.com',
+      From: document.getElementById("email").value,
+      Subject: "New Contact Form Enquiry",
+      Body: "Name: " + document.getElementById("name").value + "<br> Email: "
+          + document.getElementById("email").value + "<br> Subject: "
+          + document.getElementById("subject").value + "<br> Message: "
+          + document.getElementById("message").value
+  }).then(
+      message => alert("Mail Sent Successfully")
+  );
+}
